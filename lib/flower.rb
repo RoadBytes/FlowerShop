29a3393a057b644bfe24c9a04910432543c9617e
Jason Data <jason.data@roadbytes.me>
2016-11-24 22:05:22 +1100

    added Flower#evaluate_order(order_size)

--- lib/flower.rb
+++ lib/flower.rb
@@ -12,4 +12,60 @@ class Flower
       code == other.code &&
       batch_attributes == other.batch_attributes
   end
+
+  def evaluate_order(order_quantity)
+    bundle_subset_order(order_quantity) ||
+      bundle_approximate_order(order_quantity)
+  end
+
+  def bundle_subset_order(order_quantity)
+    superset = bundle_superset(order_quantity)
+
+    SubsetOnTarget.new(superset, order_quantity).last
+  end
+
+  def bundle_approximate_order(order_quantity)
+    approximate_order = smaller_order(order_quantity)
+    smallest_batch    = bundle_sizes.first
+
+    left_over_orders  = order_quantity - approximate_order.inject(:+)
+    until left_over_orders < 0
+      approximate_order << smallest_batch
+      left_over_orders -= smallest_batch
+    end
+
+    approximate_order.sort
+  end
+
+  private
+
+  def smaller_order(order_quantity)
+    descending_bundles_sizes = bundle_sizes.reverse
+    order                    = []
+
+    descending_bundles_sizes.each do |bundle_size|
+      bundles = order_quantity / bundle_size
+      bundles.times { order << bundle_size }
+      order_quantity -= bundle_size * bundles
+
+      break if order_quantity <= 0
+    end
+
+    order
+  end
+
+  def bundle_superset(order_quantity)
+    superset = []
+
+    bundle_sizes.each do |bundle_size|
+      bundles = order_quantity / bundle_size
+      bundles.times { superset << bundle_size }
+    end
+
+    superset.sort
+  end
+
+  def bundle_sizes
+    batch_attributes.keys.sort
+  end
 end
--- spec/flower_spec.rb
+++ spec/flower_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/flower.rb'
+require_relative '../lib/subset_on_target.rb'
 
 describe Flower do
   describe '#new(name, code, bundles)' do
@@ -23,6 +24,59 @@ describe Flower do
   end
 
   describe '#evaluate_order(order_quantity)' do
-    # TODO
+    it 'returns [10] for Roses order of 10' do
+      roses = Flower.new('Roses', 'R12', 5 => 6.99, 10 => 12.99)
+
+      order = roses.evaluate_order(10)
+
+      expect(order).to eq [10]
+    end
+
+    it 'returns [5, 10] for Roses order of 14' do
+      roses = Flower.new('Roses', 'R12', 5 => 6.99, 10 => 12.99)
+
+      order = roses.evaluate_order(14)
+
+      expect(order).to eq [5, 10]
+    end
+
+    it 'returns [3, 5, 5] for Tulips order of 13' do
+      tulips = Flower.new('Tulips', 'T58', 3 => 5.95, 5 => 9.95, 9 => 16.99)
+
+      order = tulips.evaluate_order(13)
+
+      expect(order).to eq [3, 5, 5]
+    end
+  end
+
+  describe '#bundle_subset_order(order_quantity)' do
+    it 'returns a subset order if one exists' do
+      order_quantity = 15
+      roses          = Flower.new('Rose', 'R12', 5 => 6.99, 10 => 12.99)
+
+      subset = roses.bundle_subset_order(order_quantity)
+
+      expect(subset).to eq [5, 10]
+    end
+
+    it 'returns nil if no subset exists' do
+      order_quantity = 16
+      roses          = Flower.new('Rose', 'R12', 5 => 6.99, 10 => 12.99)
+
+      subset = roses.bundle_subset_order(order_quantity)
+
+      expect(subset).to eq nil
+    end
+  end
+
+  describe '#budle_approximate_order(order_quantity)' do
+    it 'returns an approximation order if >= order_quantity' do
+      order_quantity = 16
+      roses          = Flower.new('Rose', 'R12', 5 => 6.99, 10 => 12.99)
+
+      subset = roses.bundle_approximate_order(order_quantity)
+
+      expect(subset).to eq [5, 5, 10]
+    end
   end
 end
