00cec8bb41721126c29c30d87b1a15a250cc444a
Jason Data <jason.data@roadbytes.me>
2016-11-25 06:49:27 +1100

    added Validator for user input

---
+++ lib/validator.rb
@@ -0,0 +1,43 @@
+require_relative 'flowers.rb'
+
+# Validates and Formats input from user
+class Validator
+  attr_reader :codes
+
+  def initialize
+    @codes = Flowers::CODES
+  end
+
+  def check(message)
+    check_syntax(message) ||
+      check_semantics(message)
+  end
+
+  def format(message)
+    order_quantity, order_code = message.upcase.split(/\s+/)
+    "#{order_quantity} #{order_code}"
+  end
+
+  private
+
+  def check_syntax(message)
+    unless message.match(/^\d+\s+\w+\n?\z/)
+      return 'Please follow format: #{quantity} #{code}'
+    end
+  end
+
+  def check_semantics(message)
+    order_quantity, order_code = message.split(/\s+/)
+    message = []
+
+    unless order_quantity.to_i > 0
+      message << 'Please order 1 or more flowers'
+    end
+
+    unless codes.include? order_code
+      message << "Please use one of the following order codes: #{codes}"
+    end
+
+    message.join(' ')
+  end
+end
---
+++ spec/validator_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../lib/validator.rb'
+require_relative '../lib/flowers.rb'
+
+describe Validator do
+  describe '#check(order_line)' do
+    it 'returns a syntax message if format is off' do
+      order_line = ''
+      validator  = Validator.new
+
+      message = validator.check(order_line)
+
+      expect(message).to include 'Please follow format: #{quantity} #{code}'
+    end
+
+    it 'notifies if order is too low' do
+      order_line = '0 R12'
+      validator  = Validator.new
+
+      message = validator.check(order_line)
+
+      expect(message).to include 'Please order 1 or more flowers'
+    end
+
+    it 'notifies if order code is not recognized' do
+      order_line = '0 T01'
+      validator  = Validator.new
+      codes      = validator.codes.to_s
+
+      message = validator.check(order_line)
+
+      expect(message).to include codes
+    end
+  end
+
+  describe '#format(order_line)' do
+    it 'removes excess white space and upcases' do
+      order_line = '1         r12'
+      validator  = Validator.new
+
+      message = validator.format(order_line)
+
+      expect(message).to eq '1 R12'
+    end
+  end
+end
