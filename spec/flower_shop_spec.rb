6089f1767e438f2beeabb9fceb4d2b65ff30214e
Jason Data <jason.data@roadbytes.me>
2016-11-25 10:46:14 +1100

    added multiorder integration spec

--- spec/flower_shop_spec.rb
+++ spec/flower_shop_spec.rb
@@ -11,5 +11,15 @@ describe FlowerShop do
 
       expect(STDOUT).to have_received(:puts).with(["11 orders are not possible with bundles\nhere is an approximate order\n15 R12 $19.98\n  1 x 5 $6.99\n  1 x 10 $12.99\n\n\n"])
     end
+
+    it 'prints out several orders' do
+      flower_shop = FlowerShop.new
+      allow(STDOUT).to receive(:puts)
+      allow(STDIN).to receive(:gets).and_return("10 R12\n", "1 T58\n", "9 l09\n", 'submit')
+
+      flower_shop.evaluate_order
+
+      expect(STDOUT).to have_received(:puts).with(["10 R12 $12.99\n  1 x 10 $12.99\n\n\n", "1 orders are not possible with bundles\nhere is an approximate order\n3 T58 $5.95\n  1 x 3 $5.95\n\n\n", "9 L09 $24.95\n  1 x 9 $24.95\n\n\n"])
+    end
   end
 end
