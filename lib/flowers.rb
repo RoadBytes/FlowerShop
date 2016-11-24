127926a79c00983e2dfe5083f2450d53f9c6f927
Jason Data <jason.data@roadbytes.me>
2016-11-24 15:55:29 +1100

    adding initial Flowers class

---
+++ lib/flowers.rb
@@ -0,0 +1,31 @@
+# Contains the business logic for flower bundles and prices
+class Flowers
+  attr_reader :flowers
+  def initialize
+    @flowers = {}
+  end
+
+  def self.set_up_shop
+    domain_flowers = Flowers.new
+    domain_flowers.add('Roses', 'R12', 5 => 6.99, 10 => 12.99)
+    domain_flowers.add('Lilies', 'L09', 9 => 24.95, 6 => 16.95)
+    domain_flowers.add('Tulips', 'T58', 3 => 5.95, 5 => 9.95, 9 => 16.99)
+    domain_flowers
+  end
+
+  def add(name, code, bundle_attributes)
+    flowers[code] = { name: name,
+                      code: code,
+                      bundle_attributes: bundle_attributes }
+  end
+
+  def size
+    flowers.size
+  end
+
+  def find(code)
+    flower = flowers[code]
+    raise ArgumentError, 'Flower not found' unless flower
+    flower
+  end
+end
---
+++ spec/flowers_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../lib/flowers.rb'
+
+describe Flowers do
+  describe '#add(name, code, bundle_attributes)' do
+    it 'adds a flower with appropriate attributes' do
+      flowers = Flowers.new
+
+      flowers.add('Rose', 'R12', 5 => 6.99, 10 => 12.99)
+
+      expect(flowers.size).to eq 1
+    end
+  end
+
+  describe '#find(code)' do
+    it 'returns the flower with appropriate code' do
+      flowers = Flowers.new
+      flowers.add('Rose', 'R12', 5 => 6.99, 10 => 12.99)
+      expected_flower = { name: 'Rose',
+                          code: 'R12',
+                          bundle_attributes: { 5 => 6.99, 10 => 12.99 } }
+
+      flower = flowers.find('R12')
+
+      expect(flower).to eq expected_flower
+    end
+
+    it 'raises error if code not found' do
+      flowers = Flowers.new
+
+      expect { flowers.find('R12') }.to raise_error 'Flower not found'
+    end
+  end
+
+  describe '::set_up_shop' do
+    it 'has a Roses, Lilies, and Tulips' do
+      flowers = Flowers.set_up_shop
+
+      roses  = flowers.find('R12')
+      lilies = flowers.find('L09')
+      tulips = flowers.find('T58')
+
+      expect(roses[:name]).to eq 'Roses'
+      expect(lilies[:name]).to eq 'Lilies'
+      expect(tulips[:name]).to eq 'Tulips'
+    end
+  end
+end
